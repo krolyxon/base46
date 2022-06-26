06a2b696afb8cb59eb9997857da5a7ce22a0e9e4
krolyxon <krolyxon@tutanota.com>
2022-06-26 22:05:41 +0530

    custom dark changes

--- lua/hl_themes/dark_custom.lua
+++ lua/hl_themes/dark_custom.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 M.base_30 = {
-   white = "#abb2bf",
+   white = "#e5e5e5",
    darker_black = "#000000",
    black = "#000000", --  nvim bg
    black2 = "#131313",
-   one_bg = "#282c34", -- real bg of onedark
-   one_bg2 = "#353b45",
-   one_bg3 = "#30343c",
-   grey = "#42464e",
+   one_bg = "#131313", -- real bg of onedark
+   one_bg2 = "#292929",
+   one_bg3 = "#080808",
+   grey = "#292929",
    grey_fg = "#565c64",
    grey_fg2 = "#6f737b",
    light_grey = "#6f737b",
-   red = "#d47d85",
+   red = "#cd0000",
    baby_pink = "#DE8C92",
    pink = "#ff75a0",
    line = "#2a2e36", -- for lines like vertsplit
-   green = "#A3BE8C",
-   vibrant_green = "#7eca9c",
-   nord_blue = "#81A1C1",
+   green = "#00cd00",
+   vibrant_green = "#00cd00",
+   nord_blue = "#ffffff",
    blue = "#61afef",
    yellow = "#e7c787",
    sun = "#EBCB8B",
@@ -27,10 +27,10 @@ M.base_30 = {
    teal = "#519ABA",
    orange = "#fca2aa",
    cyan = "#a3b8ef",
-   statusline_bg = "#22262e",
-   lightbg = "#2d3139",
-   lightbg2 = "#262a32",
-   pmenu_bg = "#A3BE8C",
+   statusline_bg = "#080808",
+   lightbg = "#030303",
+   lightbg2 = "#131313",
+   pmenu_bg = "#00cd00",
    folder_bg = "#61afef",
 }
 
@@ -43,13 +43,13 @@ M.base_16 = {
    base05 = "#abb2bf",
    base06 = "#b6bdca",
    base07 = "#c8ccd4",
-   base08 = "#e06c75",
+   base08 = "#cd00cd",
    base09 = "#d19a66",
-   base0A = "#e5c07b",
-   base0B = "#98c379",
+   base0A = "#cdcd00",
+   base0B = "#00ff00",
    base0C = "#56b6c2",
-   base0D = "#61afef",
-   base0E = "#c678dd",
+   base0D = "#5c5cff",
+   base0E = "#cd00cd",
    base0F = "#be5046",
 }
 
